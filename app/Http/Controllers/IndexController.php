15604f99c3e4a9423ee60d9a4b653093724589fa
alcom93 <georgeshabibou@yahoo.fr>
2023-11-25 16:22:57 -0500

    suppression des dump

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,8 +10,6 @@ class IndexController extends Controller
 {
     public function index():View{
         $doc= Document::find(10);
-        dump($doc->haspermission(2));
-        \dump($doc->permissionForUser(2));
         
         return view('homepage');    
     }
